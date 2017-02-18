e78d9057bdd09119828307ac5956b9700fa68bd3
sgr-ksmt <melodydance.k.s@gmail.com>
2017-02-18 12:35:44 +0900

    initial setup

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Suguru Kishimoto on 2/18/17.
+//  Copyright © 2017 Suguru Kishimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FormToolbar/FormToolbar.h
@@ -0,0 +1,19 @@
+//
+//  FormToolbar.h
+//  FormToolbar
+//
+//  Created by Suguru Kishimoto on 2/18/17.
+//  Copyright © 2017 Suguru Kishimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FormToolbar.
+FOUNDATION_EXPORT double FormToolbarVersionNumber;
+
+//! Project version string for FormToolbar.
+FOUNDATION_EXPORT const unsigned char FormToolbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormToolbar/PublicHeader.h>
+
+
